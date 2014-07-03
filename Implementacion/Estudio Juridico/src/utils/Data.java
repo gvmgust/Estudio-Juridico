ac38414d137deb3a7723420490e8242bf0e0ba31
Gustavo Vargas Miranda <gvmgust@gmail.com>
2014-07-03 03:43:31 -0400

    Creando tabla de Datos que se usaran en el sistema (definiendo valores constantes)

---
+++ Implementacion/Estudio Juridico/src/utils/Data.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package utils;
+
+/**
+ *
+ * @author [GVM - MABH - LCOS]
+ */
+public abstract class Data {
+    // tipos de usuario
+    public static final int SECRETARIA = 0;
+    public static final int ABOGADO = 1;
+    public static final int ADMINISTRADOR = 2;
+}
