82b709457050fb341947901c6432e50950c07585
Gustavo Vargas Miranda <gvmgust@gmail.com>
2014-07-08 02:23:43 -0400

    creacion de la clase managerTitulo (manejador del objeto titulo) capa domains

---
+++ Implementacion/Estudio Juridico/src/domains/ManagerTitulo.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package domains;
+
+import data.Titulo;
+
+/**
+ *
+ * @author Gustavo Vargas M
+ */
+public class ManagerTitulo {
+    private Titulo titulo;
+    
+    public void cargarTitulo(int id_tit){
+        // cargar de la db
+    }
+    
+    public void cargarTitulo(String titulo, String abrev){
+        // cargar los datos desde interface
+    }
+    
+}
